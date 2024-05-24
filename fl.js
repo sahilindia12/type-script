5a330c0860f12438e9ace5510545e319f148349d
sahilindia12 <indiasahil44@gmail.com>
2024-05-24 09:41:48 -0700

    this is type sript work

---
+++ fl.js
@@ -0,0 +1,19 @@
+var sahil;
+console.log('this is typre aotu run type sripte file  sripte dfgsrgdfg ');
+function go(nam, age) {
+    console.log(nam + age);
+    return (nam + age);
+}
+go('sahil', 21);
+var non = {
+    name: 'sahil',
+    age: 12,
+    acitive: true,
+    list: [12, 23]
+};
+var fom = function (dta) {
+    console.log(dta);
+    return;
+};
+fom(['sahil', 'inida ', 'rom khariya sujat harriaya']);
+var dta = {};
---
+++ fl.ts
@@ -0,0 +1,44 @@
+let sahil:string;
+
+
+console.log('this is typre aotu run type sripte file  sripte dfgsrgdfg ')
+
+type ob={
+name:string;
+age:number;
+acitive:boolean;
+list:number[]
+
+}
+
+
+ function go(nam:string,age:number):string{
+    console.log(nam+age)
+return (nam+age)
+ }
+
+ go('sahil',21)
+
+
+ let non:ob={
+name:'sahil',
+age:12,
+acitive:true,
+list:[12,23]
+ }
+ interface myface{
+
+    formdata:string,
+   
+    formno:number
+    
+ }
+
+
+ let fom=(dta:string[]):void=>{
+    console.log(dta)
+    return 
+ }
+fom(['sahil','inida ','rom khariya sujat harriaya' ])
+
+let dta={}
